472cefd9aeec2d1224ae75084814454cebe87ac1
qait-tarundwivedi <tarundwivedi@qainfotech.net>
2020-11-19 22:22:58 +0530

    added ids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-declare var require: any
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, Renderer2, ViewChildren, QueryList } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { PolicyService } from 'src/app/policy.service';
-import { Policy } from 'src/app/policy.model';
 
 @Component({
   selector: 'app-root',
@@ -13,40 +11,71 @@ import { Policy } from 'src/app/policy.model';
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild('Cengage') RootCengage: ElementRef;
+  @ViewChild('cengage_student') cengage_student: ElementRef;
+  @ViewChildren('eleBox') divs: QueryList<any>;
 
 
-   public onlyitems: any;
-   public onlyitems2 = [];
-   public submitted = false;
-   public DBData;
 
-  constructor(private db: PolicyService, dbMain: AngularFireDatabase) {
+  public onlyitems: any;
+  public onlyitems2 = [];
+  public submitted = false;
+  public DBData;
+  serializedPanes: string = '';
+
+  constructor(private db: PolicyService, private renderer: Renderer2, private elementRef: ElementRef) {
+
     // dbMain.list('/').valueChanges().subscribe((data) => {
     //   // this.onlyitems =   data;
     //   // console.log(data)
     // });
+    // this.retrieveTutorials();
   }
 
-  async ngOnInit(): Promise<void> {
+  async ngOnInit() {
     await this.retrieveTutorials();
+    await console.log('........>' + this.DBData);
   }
-  async retrieveTutorials(): Promise<void> {
-    await this.db.getAll().valueChanges().subscribe(data2 => {
+  async retrieveTutorials() {
+    return await this.db.getAll().valueChanges().subscribe(data2 => {
       // console.log('.--->'+JSON.stringify(data2+'', null, 2));
       this.onlyitems2 = data2;
-      data2.map(function (val, index) {
+      data2.map((val, index) => {
         // console.log("key : ", index, "value : ", val);
-         this.DBData =  JSON.parse(JSON.stringify(val));
+        this.DBData = JSON.parse(JSON.stringify(val));
         console.log(JSON.parse(JSON.stringify(val)));
       })
     });
-    await setTimeout(() => { console.log(this.onlyitems2); }, 3000);
+    // await setTimeout(() => { console.log(this.onlyitems2); }, 3000);
   }
-  // [ngClass]="{'label ': true, 'pass': pass, 'fail': !pass}
 
-  ngAfterViewInit() {
-    console.log('.--->'+this.RootCengage.nativeElement.id);
+
+  async ngAfterViewInit() {
+    //  await this.renderer.addClass(this.RootCengage.nativeElement, 'pass');
+    // await this.renderer.addClass(this.cengage_student.nativeElement, 'pass');
+    await console.log('.--->' + this.DBData)
+
+    await this.divs.forEach(div => console.log(div));
+    await setTimeout(() => { this.check() }, 3000);
   }
+  //   console.log ('key: ' +  key + ',  value: ' + this.DBData[key]);
+  async check() {
+    const dom: HTMLElement = this.elementRef.nativeElement;
+    const elements = dom.querySelectorAll('#wrapper span.label');
+    console.log(elements);
+    elements.forEach(ele => console.log(ele.id));
+    if (this.DBData !== undefined) {
+      elements.forEach(ele => {
+        for (let key in this.DBData) {
+          if (key === ele.id) {
+            ele.classList.add(this.DBData[key])
+            //  this.renderer.addClass(this.RootCengage.nativeElement, this.DBData[key]);
+          }
+        }
+      })
+    }
+  }
+
+
   setClasses() {
     // console.log('.--->'+this.RootCengage.nativeElement.id);
     return {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB-nt_hVTPakoruf2-yVnwFcMaP9Nkzyao",
+    authDomain: "studentflow-f0834.firebaseapp.com",
+    databaseURL: "https://studentflow-f0834.firebaseio.com",
+    projectId: "studentflow-f0834",
+    storageBucket: "studentflow-f0834.appspot.com",
+    messagingSenderId: "26694764514",
+    appId: "1:26694764514:web:148fab8a33f10428cfdbbb",
+    measurementId: "G-E4HER6EH1G"
+  }
 };
